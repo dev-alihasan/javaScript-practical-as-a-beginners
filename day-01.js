9ca2a24767fdab3e9e2c2f3a079b90a7f6ae7ef3
dev-alihasan <infomeorsendmehelp@protonmail.com>
2023-10-29 19:21:47 +0600

    js practice

---
+++ day-01.js
@@ -0,0 +1,13 @@
+// Print numbers from 1 to 100
+
+for( let nmbr= 0; nmbr <= 100; nmbr++ ){
+
+    console.log(nmbr)
+}
+
+// print a to z 
+
+for (let i = 97; i <= 122; i++) {
+    let letter = String.fromCharCode(i);
+    console.log(letter);
+}
